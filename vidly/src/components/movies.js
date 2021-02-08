2c6402fa030b949d0450d664d09006b98a334c79
BlakeAghili <peopleofksmo@gmail.com>
2021-02-07 21:18:38 -0500

    extracted table header to its own component.

--- vidly/src/components/movies.js
+++ vidly/src/components/movies.js
@@ -4,19 +4,22 @@ import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
 import ListGroup from "./common/listGroup";
 import { getGenres } from "../services/fakeGenreService";
+import MoviesTable from "./moviesTable";
+import _ from "lodash";
 
 class Movies extends Component {
   state = {
-    movies: [],
+    movies: getMovies(),
     genres: [],
     pageSize: 4,
     currentPage: 1,
-    selectedGenre: "",
+    //selectedGenre: "",
+    sortColumn: { path: "title", order: "asc" },
   };
 
   componentDidMount() {
-    const genres = [{ name: "All Genres" }, ...getGenres()];
-    this.setState({ movies: getMovies(), genres: genres });
+    const genres = [{ name: "All Genres", _id: "" }, ...getGenres()];
+    this.setState({ movies: getMovies(), genres });
   }
 
   handleGenreSelect = (genre) => {
@@ -39,6 +42,11 @@ class Movies extends Component {
   handlePageChange = (page) => {
     this.setState({ currentPage: page });
   };
+
+  handleSort = (sortColumn) => {
+    this.setState({ sortColumn });
+  };
+
   render() {
     const { length: count } = this.state.movies;
     const {
@@ -46,14 +54,19 @@ class Movies extends Component {
       pageSize,
       movies: allMovies,
       selectedGenre,
+      sortColumn,
     } = this.state;
+
     if (count === 0) return <p>There are no movies in the database</p>;
 
     const filtered =
       selectedGenre && selectedGenre._id
         ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
         : allMovies;
-    const movies = paginate(filtered, currentPage, pageSize);
+
+    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+    const movies = paginate(sorted, currentPage, pageSize);
 
     return (
       <div className="row">
@@ -66,10 +79,12 @@ class Movies extends Component {
         </div>
         <div className="col">
           <p>Showing {filtered.length} movies from database: </p>
-          <Movies
+          <MoviesTable
             movies={movies}
+            sortColumn={sortColumn}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
+            onSort={this.handleSort}
           />
           <Pagination
             itemsCount={filtered.length}
